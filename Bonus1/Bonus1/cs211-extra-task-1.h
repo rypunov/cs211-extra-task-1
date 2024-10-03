948f74f695144a0659fa9610a90ac19252561774
Денис Рыпунов <rypunov2005@bk.ru>
2024-10-03 20:54:57 +0300

    Реализована функция hours_difference

--- Bonus1/Bonus1/cs211-extra-task-1.h
+++ Bonus1/Bonus1/cs211-extra-task-1.h
@@ -2,3 +2,6 @@
 
 //Возвращает отставание time_2 от time_1
 double seconds_difference(double time_1, double time_2);
+
+//Возвращает количество часов, на которое время в секундах time_2 отстает от времени в секундах time_1
+double hours_difference(double time_1, double time_2);
--- Bonus1/Bonus1/main.cpp
+++ Bonus1/Bonus1/main.cpp
@@ -16,5 +16,14 @@ void main()
     assert(seconds_difference(1800.0, 1800.0) < DBL_EPSILON);
     cout << "Тестирование функции seconds_difference пройдено!" << endl;
 
+    // Тестирование функции hours_difference
+    assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+    assert(hours_difference(1800.0, 1800.0) < DBL_EPSILON);
+    cout << "Тестирование функции hours_difference пройдено!" << endl;
+
+
+
 	system("pause");
 }
