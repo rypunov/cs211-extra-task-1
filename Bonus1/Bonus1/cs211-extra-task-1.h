ecadb60406695ad1c7f0bcaaaaa92eb900ce8133
Денис Рыпунов <rypunov2005@bk.ru>
2024-10-03 21:03:28 +0300

    Реализована функция to_float_hours

--- Bonus1/Bonus1/cs211-extra-task-1.h
+++ Bonus1/Bonus1/cs211-extra-task-1.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <iostream>
+#include "assert.h"
+
 //Возвращает отставание time_2 от time_1
 double seconds_difference(double time_1, double time_2);
 
 //Возвращает количество часов, на которое время в секундах time_2 отстает от времени в секундах time_1
 double hours_difference(double time_1, double time_2);
+
+//Возвращает общее количество часов в указанном количестве часов, минут и секунд
+double to_float_hours(int hours, int minutes, int seconds);
--- Bonus1/Bonus1/main.cpp
+++ Bonus1/Bonus1/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include "assert.h"
 #include "cs211-extra-task-1.h"
 
 using namespace std;
@@ -23,7 +21,11 @@ void main()
     assert(hours_difference(1800.0, 1800.0) < DBL_EPSILON);
     cout << "Тестирование функции hours_difference пройдено!" << endl;
 
-
+    // Тестирование функции to_float_hours
+    assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+    assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+    assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
+    cout << "Тестирование функции to_float_hours пройдено!" << endl;
 
 	system("pause");
 }
